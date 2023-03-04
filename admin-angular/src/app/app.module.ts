d9b9dea6d62c595de04d37c79fe902ac50690d3f
islam kamel <islam.kamel@agr.svu.edu.eg>
2023-03-05 00:43:16 +0200

    Added api request

--- admin-angular/src/app/app.module.ts
+++ admin-angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AdminDashboardComponent} from "./Components/dashboard/admin-dashboard.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
