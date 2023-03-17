586b965cb06b8e04ec8083021d1ce9107077bdee
Islam Kamel <islam.kamel@agr.svu.edu.eg>
2023-03-17 20:55:27 +0200

    Update package

--- socket-server/server.js
+++ socket-server/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
-const socket = require("socket.io");
-const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+const cors = require("cors");
 const PORT = 3008;
 
+app.use(cors())
 app.get("/", (req, res) => {
     res.send("Hello, Socket")
 })
-app.listen(PORT)
+
+io.on("connection", (socket) => {
+    console.log("New connect", socket.id);
+    socket.on("message", args => {
+        console.log(args)
+    });
+})
+
+io.listen(PORT, () => {
+    console.log('listening on localhost:3000');
+});
